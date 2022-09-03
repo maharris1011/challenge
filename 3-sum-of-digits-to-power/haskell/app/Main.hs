e55acaa167a08c9f9f18da7a36f62e7585037584
mark harris <ma_harris1011@hotmail.com>
2022-09-03 12:06:20 -0400

    more idiomatic haskell

--- 3-sum-of-digits-to-power/haskell/app/Main.hs
+++ 3-sum-of-digits-to-power/haskell/app/Main.hs
@@ -6,21 +6,32 @@ import System.Environment
 listOfDigits :: Integer -> [Integer]
 listOfDigits x
   | x < 10 = [x]
-  | otherwise = listOfDigits (x `div` 10) ++ [(x `mod` 10)]
+  | otherwise = (x `mod` 10) : listOfDigits (x `div` 10)
 
--- minimum number where 10^d > 9^5d
-maxNumber :: Integer -> Integer -> Integer
-maxNumber d e
-  | 10^d-1 > ((9^e) * d) = (9^e) * d
-  | otherwise = maxNumber (d+1) e
+-- first sum of exponents where largest n-digit number > sum of 9s to exponent
+maxNumber :: Integer -> Integer
+maxNumber e = maxNumber' 2 (9^e) (10^2)
 
-findMatchesOfPower :: Integer -> [Integer]
-findMatchesOfPower e = do
-  let minimum = maxNumber 2 e
-  filter p [10..minimum]
-    where p x = (sum (map (^e) (listOfDigits x))) == x
+maxNumber' :: Integer -> Integer -> Integer -> Integer
+maxNumber' d e accum
+  | e * d < accum = e * d
+  | otherwise = maxNumber' (d+1) e (accum * 10)
+
+digitsToPower :: Integer -> Integer -> [Integer]
+digitsToPower x e = map (^e) (listOfDigits x)
+
+sumOfDigitsToPower num e = sum(digitsToPower num e)
+
+candidatePairs :: Integer -> [(Integer, Integer)]
+candidatePairs e = do
+  let num = (maxNumber e)
+  [(x, sumOfDigitsToPower x e) | x <- [10..num]]
+
+matchingPairs :: Integer -> [(Integer, Integer)]
+matchingPairs e = [(x, y) | (x, y) <- (candidatePairs e), x == y]
 
 main :: IO ()
 main = do
   args <- getArgs
-  print (findMatchesOfPower (read $ head args :: Integer))
+  let exponent = (read $ head args :: Integer)
+  print (map fst (matchingPairs exponent))
