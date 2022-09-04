dd63ab14e5743c24d5bddc80483a5beb608aef94
mark harris <ma_harris1011@hotmail.com>
2022-09-04 11:19:25 -0400

    format for js standards

--- 3-sum-of-digits-to-power/nodejs/digitsum.js
+++ 3-sum-of-digits-to-power/nodejs/digitsum.js
@@ -1,12 +1,14 @@
-
 let digitSum = (x, n) => {
-  return x.toString().split('').map((val) => BigInt(val ** n))
+  return x
+    .toString()
+    .split("")
+    .map((digit) => BigInt(digit ** n))
     .reduce((sum, val) => BigInt(sum) + BigInt(val), 0)
 }
 
 let minNumber = (d, n) => {
-  let maxNum = BigInt((9**n)*d)
-  if (BigInt(10**d-1) > maxNum) {
+  let maxNum = BigInt(9 ** n * d)
+  if (BigInt(10 ** d - 1) > maxNum) {
     return maxNum
   } else {
     return minNumber(d + 1, n)
@@ -15,13 +17,16 @@ let minNumber = (d, n) => {
 
 function* range(start, end) {
   for (let i = start; i <= end; i++) {
-    yield i;
+    yield i
   }
 }
 
 //var matches = []
-let minNum = minNumber(2,5)
-let matches = [...range(10, minNum)].filter(i => digitSum(i, 5) === BigInt(i))
+let minNum = minNumber(2, 5)
+let matches = [...range(10, minNum)].filter((i) => digitSum(i, 5) === BigInt(i))
 
-console.log(matches.join(' + ') + ' = ' + 
-            matches.reduce((sum, val) => BigInt(sum)+BigInt(val), 0))
+console.log(
+  matches.join(" + ") +
+    " = " +
+    matches.reduce((sum, val) => BigInt(sum) + BigInt(val), 0)
+)
