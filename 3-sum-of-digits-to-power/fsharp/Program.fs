d9773c8b9beecc9a0d17b25d7742206feee317fe
mark harris <ma_harris1011@hotmail.com>
2022-11-08 22:42:31 -0500

    optimize for partial sums

--- 3-sum-of-digits-to-power/fsharp/Program.fs
+++ 3-sum-of-digits-to-power/fsharp/Program.fs
@@ -2,32 +2,40 @@
 
 module SumOfDigits =
     let findMatchesOfPower (exponent: int) =
-        let digitsToPower = [| for i in 0..9 -> int (float i ** exponent) |]
+        let digitsToPower = [ for i in 0UL .. 9UL -> uint64 (float i ** exponent) ]
 
         let rec maxNumber (digit: int) =
-            let maxByDigit = int (10.0 ** digit) - 1
-            let maxSumDigits = int (9.0 ** exponent) * int digit
+            let maxByDigit = uint64 (10.0 ** digit) - 1UL
+            let maxSumDigits = uint64 (9.0 ** exponent) * uint64 digit
 
             match (maxByDigit, maxSumDigits) with
             | (var1, var2) when var1 > var2 -> var2
             | otherwise -> maxNumber (digit + 1)
 
-        let rec listOfDigits (x: int) =
+        let rec listOfDigits (x: uint64) =
             match x with
-            | (var1) when var1 < 10 -> [ var1 ]
-            | otherwise -> [ (x % 10) ] @ listOfDigits (x / 10)
+            | (var1) when var1 < 10UL -> [ var1 ]
+            | otherwise -> [ (x % 10UL) ] @ listOfDigits (x / 10UL)
 
-        let sumDigitsToPower x =
+        let sumDigitsToPower (x: uint64) =
             listOfDigits (x)
-            |> List.sumBy (fun x -> digitsToPower[x])
+            |> List.sumBy (fun digit -> digitsToPower[int digit])
 
         let topOfRange = maxNumber (2)
-        let candidates = [ 10..topOfRange ]
+        let candidates = [ 10UL .. 10UL .. topOfRange ]
 
-        candidates
-        |> List.filter (fun x -> sumDigitsToPower (x) = x)
+        let memoSumDigitsToPower (x: uint64) =
+            let baseSum = sumDigitsToPower (x)
+
+            for digit in 0UL .. 9UL do
+                if (x + digit) = (baseSum + digitsToPower[int digit]) then
+                    printf "%u, " (x + uint64 digit)
+
+        candidates |> List.map memoSumDigitsToPower
 
 [<EntryPoint>]
 let main args =
-    printfn "%A" (SumOfDigits.findMatchesOfPower (int args[0]))
+    SumOfDigits.findMatchesOfPower (int args[0])
+    |> ignore
+
     0
