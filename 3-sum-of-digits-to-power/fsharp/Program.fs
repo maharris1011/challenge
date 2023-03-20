346853ba4741ff33f719faff94740ab3c1369e24
mark harris <ma_harris1011@hotmail.com>
2023-03-19 22:29:45 -0400

    idiomatic F# code

--- 3-sum-of-digits-to-power/fsharp/Program.fs
+++ 3-sum-of-digits-to-power/fsharp/Program.fs
@@ -1,66 +1,48 @@
 ﻿open System
 
 module SumOfDigits =
+    let maxNumber (exponent: int) : int64 =
+        let nineToTheExponent = int64 (Math.Pow(9.0, exponent))
 
-    let sumOfDigitsRaisedToPower (n: uint64) (p: int) =
-        let rec loop (m: uint64) acc =
+        let rec loop (digit: int) (maxByDigit: int64) =
+            let maxSumDigits = nineToTheExponent * int64 (digit)
+
+            match ((maxByDigit - 1L), maxSumDigits) with
+            | (var1, var2) when var1 > var2 -> var2
+            | _ -> loop (digit + 1) (maxByDigit * 10L)
+
+        loop 2 100
+
+    let sumOfDigitsRaisedToPower (n: int64) (p: int) =
+        let rec loop (m: int64) acc =
             match m with
-            | 0UL -> acc
-            | _ -> loop (m / 10UL) (acc + int64 (Math.Pow(float m % 10.0, float p)))
+            | 0L -> acc
+            | _ -> loop (m / 10L) (acc + int64 (Math.Pow(float (m % 10L), p)))
 
         loop n 0L
 
-    let maxNumber (exponent: int) : uint64 =
-        let nineToTheExponent = 9.0 ** exponent
+    let narcNumbersInNextTen (n: int64) (p: int) =
+        let baseSum = sumOfDigitsRaisedToPower n p
 
-        let rec loop (digit: int) =
-            let maxByDigit = uint64 (10.0 ** digit) - 1UL
-            let maxSumDigits = uint64 (nineToTheExponent) * uint64 digit
-
-            match (maxByDigit, maxSumDigits) with
-            | (var1, var2) when var1 > var2 -> var2
-            | otherwise -> loop (digit + 1)
-
-        loop 2
+        [ 0L .. 9L ]
+        |> List.map (fun d -> (n + d, baseSum + int64 (Math.Pow(float d, p))))
+        |> List.filter (fun (x, y) -> x = y)
+        |> List.map (fun (x, y) -> x)
 
     let findNumbersWithSumOfDigitsRaisedToPower p =
-        [ 10UL .. maxNumber (p) ]
-        |> List.filter (fun n -> sumOfDigitsRaisedToPower n p = int64 n)
+        let maxNum = maxNumber p
 
+        let rec loop (m: int64) (p: int) acc =
+            match m with
+            | var1 when var1 > maxNum -> acc
+            | _ -> loop (m + 10L) p (acc @ (narcNumbersInNextTen m p))
 
-let rec maxNumber (digit: int, exponent: int) =
-    let maxByDigit = uint64 (10.0 ** digit) - 1UL
-    let maxSumDigits = uint64 (9.0 ** exponent) * uint64 digit
-
-    match (maxByDigit, maxSumDigits) with
-    | (var1, var2) when var1 > var2 -> var2
-    | otherwise -> maxNumber (digit + 1, exponent)
-
-// do power math only once
-let digitsAtPower (e: int) =
-    [ 0..9 ] |> Seq.map (fun d -> float d ** e)
-
-// Returns the sum of the cubes of the digits of a number
-let sumCubes (n: uint64, e: int) =
-    n
-    |> string
-    |> Seq.map (fun c -> int c - 48)
-    |> Seq.map (fun x -> digitsAtPower (e) |> Seq.item x)
-    |> Seq.sum
-    |> uint64
-
-// Returns the list of numbers for which the sum of the cubes of their digits equals the original number
-let specialNumbers e =
-    let topOfRange = maxNumber (2, e)
-
-    [ 11UL .. topOfRange ]
-    |> Seq.filter (fun n -> n = sumCubes (n, e))
+        loop 10L p []
 
 [<EntryPoint>]
 let main args =
-    specialNumbers (int args[0])
-    |> Seq.toList
-    |> printf "%A"
+    SumOfDigits.findNumbersWithSumOfDigitsRaisedToPower (int args[0])
+    |> printf "%A\n"
     |> ignore
 
     0
