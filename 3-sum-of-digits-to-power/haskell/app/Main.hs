a80d3a8fd8793cedc2e0579d80dfbe6e3697142c
Mark Harris <ma_harris1011@hotmail.com>
2023-03-20 23:31:22 -0400

    use chatgpt code. its faster

--- 3-sum-of-digits-to-power/haskell/app/Main.hs
+++ 3-sum-of-digits-to-power/haskell/app/Main.hs
@@ -4,13 +4,13 @@ import System.Environment
 import Data.Char (digitToInt)
 
 -- Returns the sum of the cubes of the digits of a number
-sumCubes :: Int -> Int
-sumCubes x = sum . map (^5) . map digitToInt . show
+sumCubes :: Int -> Int -> Int
+sumCubes x e = (sum . map (^e) . map digitToInt . show) x
 
--- Returns the list of numbers for which the sum of the cubes of their digits equals the original number
+-- Returns the list of numbers for which the sum of the cubes
+-- of their digits equals the original number
 specialNumbers :: Int -> [Int]
-specialNumbers e = [x | x <- [1..maxNumber e], x == sumCubes x]
-
+specialNumbers e = [x | x <- [10..maxNumber e], x == sumCubes x e]
 
 -- first sum of exponents where largest n-digit number > sum of 9s to exponent
 maxNumber :: Int -> Int
@@ -21,26 +21,8 @@ maxNumber' numDigits nineToE accum
   | (nineToE * numDigits) < (accum - 1) = nineToE * numDigits
   | otherwise = maxNumber' (numDigits + 1) nineToE (accum * 10)
 
--- turn integer number into list of integers
-listOfDigits :: Int -> [Int]
-listOfDigits x
-  | x < 10 = [(x)]
-  | otherwise = ((x `mod` 10)) : listOfDigits (x `div` 10)
-
-sumOfDigitsToPower :: Int -> [Int] -> Int
-sumOfDigitsToPower num l =
-  let toPower i = l !! (fromIntegral i)
-      digitsToPower = map (toPower) (listOfDigits num)
-  in sum $ digitsToPower
-
-matchingPairs :: Int -> [(Int, Int)]
-matchingPairs e = [(x, y) | (x, y) <- candidatePairs, x == y]
-  where l = map (^e) [0..9]
-        candidatePairs = [(x, sumOfDigitsToPower x l) | x <- [10..(maxNumber e)]]
-
 main :: IO ()
 main = do
   args <- getArgs
   let exponent = (read $ head args :: Int)
-  -- print (specialNumbers exponent)
-  print (map fst (matchingPairs exponent))
+  print (specialNumbers exponent)
