e326d67885c9830a462794370f1863da3eac8323
Mark Harris <ma_harris1011@hotmail.com>
2023-03-22 22:58:12 -0400

    optimize the Haskell

--- 3-sum-of-digits-to-power/haskell/app/Main.hs
+++ 3-sum-of-digits-to-power/haskell/app/Main.hs
@@ -5,12 +5,26 @@ import Data.Char (digitToInt)
 
 -- Returns the sum of the cubes of the digits of a number
 sumCubes :: Int -> Int -> Int
-sumCubes x e = (sum . map (^e) . map digitToInt . show) x
+sumCubes x e = do
+  (sum . map (^e) . map digitToInt . show) x
+
+-- Returns numbers in the next 10 for which the sum of the
+-- cubes of their digits equals the number itself
+specialNumbersInNext10 :: Int -> Int -> [Int]
+specialNumbersInNext10 e n = do
+  [n + x | x <- [0..9], n + x == baseSum + (x ^ e)]
+  where baseSum = sumCubes n e
+
+specialNumbers' :: Int -> Int -> Int -> [Int]
+specialNumbers' n e maxNumber
+  | n > maxNumber = []
+  | otherwise = specialNumbers' (n+10) e maxNumber ++ specialNumbersInNext10 e n
 
 -- Returns the list of numbers for which the sum of the cubes
 -- of their digits equals the original number
 specialNumbers :: Int -> [Int]
-specialNumbers e = [x | x <- [10..maxNumber e], x == sumCubes x e]
+specialNumbers e = do
+  specialNumbers' 10 e (maxNumber e)
 
 -- first sum of exponents where largest n-digit number > sum of 9s to exponent
 maxNumber :: Int -> Int
