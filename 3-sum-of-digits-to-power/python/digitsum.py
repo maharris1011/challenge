9bea142c73b8605a8813c04253ae479117b0e1ea
mark harris <ma_harris1011@hotmail.com>
2023-03-25 17:47:59 -0400

    rust implementation

--- 3-sum-of-digits-to-power/python/digitsum.py
+++ 3-sum-of-digits-to-power/python/digitsum.py
@@ -6,38 +6,20 @@ cache = {}
 
 def max_num(exponent):
     digit = 2
-    maximum = 10
-    max_digit_sum = sum_digits(99)
-    nine_to_the_exponent = pow(9, exponent)
-    while (maximum - 1) < max_digit_sum:
-        maximum *= 10
-        max_digit_sum = nine_to_the_exponent * digit
+    while (10**digit - 1) < (9**exponent * digit):
         digit += 1
-    return max_digit_sum
+    return (9**exponent * digit)
 
 
-def sum_digits_rec(num):
-    if num in cache:
-        return cache[num]
-    cache[num] = sum_digits_rec(num // 10) + sum_digits_rec(num % 10)
-    return cache[num]
-
-
-def sum_digits(num):
-    total = 0
-    while num > 0:
-        digit = num % 10
-        total += cache[digit]
-        num = num // 10
-    return total
+def sum_of_digits_raised_to_power(n, p):
+    return sum(cache[int(d)] for d in str(n))
 
 
 def matching_numbers(maxnum, exponent):
-    step = 10
     numlist = []
     # find matching numbers from 10..maxnum
-    for num in range(10, maxnum + step, step):
-        base_sum = sum_digits_rec(num)
+    for num in range(10, maxnum + 10, 10):
+        base_sum = sum_of_digits_raised_to_power(num, exponent)
         for j in range(10):
             if (base_sum + cache[j]) == (j + num):
                 numlist.append(j + num)
@@ -46,11 +28,13 @@ def matching_numbers(maxnum, exponent):
 
 def main(argv):
     exponent = 5
+
     try:
         opts, args = getopt.getopt(argv, "hp:", ["help", "power"])
     except getopt.GetoptError:
         print('digitsum.py -p <exponent>')
         sys.exit(2)
+
     for opt, arg in opts:
         if opt in ('-h', '--help'):
             print('digitsum.py -p <exponent>')
@@ -58,6 +42,7 @@ def main(argv):
         elif opt in ("-p", "--power"):
             print('arg = ', arg)
             exponent = int(arg)
+
     for x in range(10):
         cache[x] = pow(x, exponent)
     maxnum = max_num(exponent)
