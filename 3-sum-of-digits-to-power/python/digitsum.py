100060926b9640568fb5d63059a40e341625666b
mark harris <ma_harris1011@hotmail.com>
2023-03-26 20:52:26 -0400

    idiomatic python

--- 3-sum-of-digits-to-power/python/digitsum.py
+++ 3-sum-of-digits-to-power/python/digitsum.py
@@ -4,22 +4,24 @@ import getopt
 cache = {}
 
 
-def max_num(exponent):
+def max_num(p):
     digit = 2
-    while (10**digit - 1) < (9**exponent * digit):
+    while (10**digit - 1) < (9**p * digit):
         digit += 1
-    return (9**exponent * digit)
+    return (9**p * digit)
 
 
 def sum_of_digits_raised_to_power(n, p):
     return sum(cache[int(d)] for d in str(n))
 
 
-def matching_numbers(maxnum, exponent):
+def matching_numbers(p):
     numlist = []
+    maxnum = max_num(p)
+
     # find matching numbers from 10..maxnum
     for num in range(10, maxnum + 10, 10):
-        base_sum = sum_of_digits_raised_to_power(num, exponent)
+        base_sum = sum_of_digits_raised_to_power(num, p)
         for j in range(10):
             if (base_sum + cache[j]) == (j + num):
                 numlist.append(j + num)
@@ -27,27 +29,24 @@ def matching_numbers(maxnum, exponent):
 
 
 def main(argv):
-    exponent = 5
 
     try:
         opts, args = getopt.getopt(argv, "hp:", ["help", "power"])
     except getopt.GetoptError:
-        print('digitsum.py -p <exponent>')
+        print('digitsum.py -p <p>')
         sys.exit(2)
 
     for opt, arg in opts:
         if opt in ('-h', '--help'):
-            print('digitsum.py -p <exponent>')
+            print('digitsum.py -p <p>')
             sys.exit()
         elif opt in ("-p", "--power"):
             print('arg = ', arg)
-            exponent = int(arg)
-
-    for x in range(10):
-        cache[x] = pow(x, exponent)
-    maxnum = max_num(exponent)
-    matching = matching_numbers(maxnum, exponent)
-    print(exponent, ":", maxnum, ": ", matching)
+            p = int(arg)
+            for x in range(10):
+                cache[x] = pow(x, p)
+            matching = matching_numbers(p)
+            print(p, ": ", matching)
 
 
 if __name__ == "__main__":
