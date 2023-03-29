f473350b478dd838ff5125c722de819766fc2e50
Mark Harris <ma_harris1011@hotmail.com>
2023-03-29 10:26:24 -0400

    optimize look/feel of code

--- 3-sum-of-digits-to-power/csharp/cs-sum-of-digits-to-power/Program.cs
+++ 3-sum-of-digits-to-power/csharp/cs-sum-of-digits-to-power/Program.cs
@@ -22,12 +22,9 @@
         {
             var digit = 2;
             ulong maxDigitSum = _nineToTheExponent * (ulong)digit;
-            ulong maximum = 10;
-            while (maximum < maxDigitSum)
+            for (ulong maximum = 100; maximum - 1 < maxDigitSum; digit++, maximum *= 10)
             {
-                maximum = maximum * 10;
                 maxDigitSum = _nineToTheExponent * (ulong)digit;
-                digit += 1;
             }
             return maxDigitSum;
         }
@@ -35,34 +32,16 @@
         public ulong SumDigits(ulong num)
         {
             ulong digitSum = 0;
-            ulong number = num;
-            while (number != 0)
+            for (ulong number = num; number != 0; number /= 10 )
             {
-                ulong digit = number % 10;
-                number = number / 10;
-                digitSum += _cache[digit];
+                digitSum += _cache[number % 10];
             }
             return digitSum;
         }
 
-
-        public void MatchingNumbers(ulong maxNum)
+        public IEnumerable<ulong> ProduceMatchingNumbers()
         {
-            for (ulong i = 10; i <= (maxNum + 10); i += 10)
-            {
-                ulong base_sum = SumDigits(i);
-                for (ulong j = 0; j < 10; j++)
-                {
-                    if ((i + j) == (base_sum + _cache[j]))
-                    {
-                        Console.Write($"{(i + j)}, ");
-                    }
-                }
-            }
-        }
-
-        public IEnumerable<ulong> ProduceMatchingNumbers(ulong maxNum)
-        {
+            ulong maxNum = minNumber();
             for (ulong i = 10; i <= (maxNum + 10); i += 10)
             {
                 ulong base_sum = SumDigits(i);
@@ -86,9 +65,8 @@
                 exponent = int.Parse(args[0]);
             }
             DigitFinder df = new DigitFinder(exponent);
-            ulong minNum = df.minNumber();
-            Console.Write(minNum + ": " + exponent + ": ");
-            foreach (ulong num in df.ProduceMatchingNumbers(minNum))
+            Console.Write(exponent + ": ");
+            foreach (ulong num in df.ProduceMatchingNumbers())
             {
                 Console.Write(num + ", ");
             }
