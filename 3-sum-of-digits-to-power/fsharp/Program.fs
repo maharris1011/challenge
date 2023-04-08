df58269ef1bb684f81376d561de12a76d5f17716
mark harris <ma_harris1011@hotmail.com>
2023-04-08 10:05:07 -0400

    significantly simplify the code

--- 3-sum-of-digits-to-power/fsharp/Program.fs
+++ 3-sum-of-digits-to-power/fsharp/Program.fs
@@ -13,36 +13,24 @@ module SumOfDigits =
 
         loop 2 100
 
-    let sumOfDigitsRaisedToPower (cache: int64 list) (n: int64) (p: int) =
-        let rec loop (m: int64) acc =
-            match m with
-            | 0L -> acc
-            | _ -> loop (m / 10L) (acc + int64 (cache[int (m % 10L)]))
-
-        loop n 0L
-
-    let narcNumbersInNextTen (cache: int64 list) (p: int) (n: int64) =
-        let baseSum = sumOfDigitsRaisedToPower cache n p
-
-        [ 0L .. 9L ]
-        |> List.filter (fun d -> (n + d) = (baseSum + cache[int (d)]))
-        |> List.map (fun d -> n + d)
+    let sumOfDigitsRaisedToPower (cache: int64 array) (n: int64) =
+        (string n).ToCharArray()
+        |> Array.fold (fun acc c -> acc + cache[int c - int '0']) 0L
 
     let findNumbersWithSumOfDigitsRaisedToPower p =
         let maxNum = maxNumber p
 
         let cache =
-            [ 0L .. 9L ]
-            |> List.map (fun d -> int64 (Math.Pow(float d, p)))
+            [| 0L .. 9L |]
+            |> Array.map (fun d -> int64 (Math.Pow(float d, p)))
 
-        let narcNumbers = narcNumbersInNextTen cache p
+        let sumDigits = sumOfDigitsRaisedToPower cache
 
-        let rec loop (m: int64) (p: int) acc =
-            match m with
-            | var1 when var1 > maxNum -> acc
-            | _ -> loop (m + 10L) p (acc @ (narcNumbers m))
+        seq {
+            for n in 10L .. 10L .. maxNum do
+                if n = sumDigits n then n
+        }
 
-        loop 10L p []
 
 [<EntryPoint>]
 let main args =
