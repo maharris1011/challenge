96e3add10dca5d468395e85b509030ffae86e982
mark harris <ma_harris1011@hotmail.com>
2023-04-08 23:23:09 -0400

    ruby optimizations

--- 3-sum-of-digits-to-power/ruby/digitsum.rb
+++ 3-sum-of-digits-to-power/ruby/digitsum.rb
@@ -1,15 +1,14 @@
-
 def maxNumberSearch(e)
   d = 2
-  while ((10 ** d) - 1 < (9 ** e) * d) 
+  while ((10 ** d) - 1 < (9 ** e) * d)
     d = d + 1
   end
   return (9 ** e) * d
 end
 
-def digitsum(n,e)
+def digitsum(n, e)
   sum = 0
-  while n != 0 do
+  while n != 0
     sum = sum + e[(n % 10)]
     n = n / 10
   end
@@ -18,21 +17,19 @@ end
 
 def find_in_10(num, e)
   baseSum = digitsum(num, e)
-  matching = (0..9).select { |i|
-    (num + i) == (baseSum + e[i])
-  }
-  .map { |match| 
-    num + match
-  }
-  return matching
+  return (0..9).select { |i|
+           (num + i) == (baseSum + e[i])
+         }
+           .map { |match|
+           num + match
+         }
 end
-
 
 ARGV.each do |power|
   e = power.to_i
-  exps = (0..9).map {|i| i ** e }
+  exps = (0..9).map { |i| i ** e }
   rg = (10..maxNumberSearch(e)).step(10).map { |num|
-    find_in_10(num,exps)
+    find_in_10(num, exps)
   }.flatten
   printf "#{e}: #{rg}\n"
 end
