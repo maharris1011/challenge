d7f2333ec072ce9c3fe7bda5b726488c969fb120
mark harris <ma_harris1011@hotmail.com>
2023-06-08 11:53:02 -0400

    idiomatic elixir

--- 3-sum-of-digits-to-power/elixir/sum-digits.exs
+++ 3-sum-of-digits-to-power/elixir/sum-digits.exs
@@ -1,6 +1,6 @@
 defmodule SumDigits do
 
-  def max_number(d, maxD, e) do
+  defp max_number(d, maxD, e) do
     cond do
       (maxD - 1) > (e * d) ->
         trunc(e * d)
@@ -9,32 +9,28 @@ defmodule SumDigits do
     end
   end
 
-  def max_number(e) do
-    max_number(2, 100, :math.pow(9,e))
+  defp max_number(e) do
+    max_number(2, 100, :math.pow(9, e))
   end
 
-  def sum_of_digits_to_power(n, e) do
-    Enum.reduce(Integer.digits(n), 0, fn d, acc ->
-      acc + :math.pow(d, e)
-    end)
+  defp sum_of_digits_to_power(n, powermap) do
+    Integer.digits(n) |> Enum.reduce(0, fn d, acc -> acc + Map.get(powermap, d) end)
   end
 
-  def numbers_in_next_10(p, n) do
-    baseSum = sum_of_digits_to_power(n, p)
-    Enum.to_list(0..9)
-      |> Enum.filter(fn d -> (n + d) == baseSum + :math.pow(d, p) end)
-      |> Enum.map(fn d -> n + d end)
-  end
-
-  def numbers_equal_to_sum_of_digits_raised_to_power(start, p, max, acc) do
-    cond do
-      (start > max) -> acc
-      true -> numbers_equal_to_sum_of_digits_raised_to_power(start + 10, p, max, numbers_in_next_10(p, start) ++ acc)
+  def numbers_in_next_10(n, powermap) do
+    baseSum = sum_of_digits_to_power(n, powermap)
+    for d <- 0..9, (n+d) == baseSum + Map.get(powermap, d) do 
+      n + d
     end
   end
 
+  def numbers_equal_to_sum_of_digits_raised_to_power(start, p, max) do
+    {_, powermap} = Enum.to_list(0..9) |> Enum.map_reduce(%{}, fn x, acc -> {x, Map.put(acc, x, :math.pow(x,p))} end)
+    Enum.to_list(start..max//10) |> Enum.map(fn base -> numbers_in_next_10(base, powermap) end) |> List.flatten
+  end
+
   def find_for_power(p) do
-    IO.inspect numbers_equal_to_sum_of_digits_raised_to_power(10, p, max_number(p), []), label: "for power #{p}"
+    IO.inspect numbers_equal_to_sum_of_digits_raised_to_power(10, p, max_number(p)), label: "for power #{p}"
   end
 end
 
